1456346f3598362c1c46c5b7691b998e2a4f588c
Edward Timmer <edtimmer@gmail.com>
2026-02-25 02:49:36 -0500

    update hornet model

--- src/components/HornetGroup/Hornet.tsx
+++ src/components/HornetGroup/Hornet.tsx
@@ -26,7 +26,7 @@ const Hornet = forwardRef<THREE.Group, Props>(
     useImperativeHandle(ref, () => groupRef.current!);
 
     const { animations, scene } = useGLTF(
-      "/models/hornet19.glb"
+      "/models/hornet27.glb"
     );
 
     const { actions } = useAnimations(animations, parentGroupRef);
--- src/components/HornetGroup/index.tsx
+++ src/components/HornetGroup/index.tsx
@@ -9,8 +9,7 @@ const HornetGroup = () => {
   return (
     <group ref={outerGroupRef}>
       <group ref={platformGroupRef} position={[0, -0.16, 0]}>
-        <Hornet position={[-0.014, 0, 0]} rotation={[0, 0, 0]} scale={1.3} />
-        {/* <Platform position={[0, 0, 0]} rotation={[0.2, 0, 0]} scale={0.1} /> */}
+        <Hornet position={[0, 0.16, 0]} rotation={[0, 0, 0]} scale={1.3} />
         <directionalLight position={[-1.3, -0.2, 0.5]} intensity={1.0} />
         <directionalLight position={[1.3, -0.2, 0.5]} intensity={1.0} />
       </group>
